422a8719159778d45b9ecb3b60af968d99860089
Lorenzo Bartolini <lori.barto@live.it>
2022-10-10 09:39:28 +0200

    divide typings in category files

--- form.d.ts
+++ form.d.ts
@@ -1,83 +1,8 @@
 // https://docs.microsoft.com/en-us/power-apps/developer/model-driven-apps/clientapi/reference
 export { };
 
-type Override<T, U> = Omit<T, keyof U> & U;
-
 declare global
 {
-    const Xrm: {
-        Navigation: Navigation;
-        WebApi: WebApi;
-        Utility: Utility;
-        /** @deprecated use formContext instead */
-        Page: any;
-    };
-
-    type Utility = {
-        showProgressIndicator: (message: string) => void;
-        closeProgressIndicator: () => void;
-        getGlobalContext: () => GlobalContext;
-    };
-
-    type Navigation = {
-        openAlertDialog: OpenAlertDialog;
-        openConfirmDialog: OpenConfirmDialog;
-        openForm: (entityFormOptions: EntityFormOptions, formParameters: any) => Promise<{ savedEntityReference: { entityType: string, id: string, name: string; }[] | null; }>;
-    };
-
-    type EntityFormOptions = {
-        entityName: string;
-        entityId?: string;
-        formId?: string;
-        cmdbar?: boolean;
-        createFromEntity?: { entityType: string, id: string, name?: string; };
-        height?: number;
-        width?: number;
-        navbar?: "on" | "off" | "entity";
-        openInNewWindow?: boolean;
-        windowPosition?: 1 | 2;
-        relationship?: { attributeName: string, name: string, navigationPropertyName: string, relationshipType: 0 | 1, roleType: 1 | 2; };
-        selectedStageId?: string;
-        useQuickCreateForm?: boolean;
-    };
-
-    type WebApi = {
-        online: any;
-        execute: (request: { getMetadata: () => any; }) => Promise<{ headers: any, ok: boolean, status: number, statusText: string, url: string, json: Promise<any>, text: Promise<string>; }>;
-        createRecord: (entityLogicalName: string, data: any) => Promise<{ entityType: string, id: string; }>;
-        retrieveRecord: (entityLogicalName: string, id: string, options?: string) => Promise<{ [key: string]: any; }>;
-        retrieveMultipleRecords: (entityLogicalName: string,
-            options?: string,
-            maxPageSize?: number,
-            successCallback?: () => any,
-            errorCallback?: () => any) => Promise<{ entities: any[]; }>;
-        updateRecord: (entityLogicalName: string, id: string, data: any) => Promise<{ entityType: string, id: string; }>;
-        deleteRecord: (entityLogicalName: string, id: string) => Promise<{ entityType: string, id: string, name: string; }>;
-    };
-
-    type AlertStrings = { confirmButtonLabel?: string, text: string, title?: string; };
-    type DialogOptions = { height?: number, width?: number; };
-
-    type ConfirmStrings = { cancelButtonLabel?: string, confirmButtonLabel?: string, subtitle?: string, text: string, title?: string; };
-
-    type OpenAlertDialog = (alertStrings: AlertStrings, alertOptions?: DialogOptions, closeCallback?: () => any, alertCallback?: () => any) => Promise<void>;
-
-    type ConfirmDialogResponse = { confirmed: boolean; };
-    type OpenConfirmDialog = (confirmStrings: ConfirmStrings, confirmOptions?: DialogOptions, successCallback?: () => any, errorCallback?: () => any) => Promise<ConfirmDialogResponse>;
-
-    type UserSettings = {
-        roles: {
-            get: () => { id: string, name: string; }[];
-        };
-        userId: string;
-        userName: string;
-    };
-
-    type GlobalContext = {
-        client: { getClient: () => "Web" | "Outlook" | "Mobile"; };
-        userSettings: UserSettings;
-    };
-
     type ExecutionContext<TExtraArgs = void, TEntity extends AnyEntity = AnyEntity> = {
         getFormContext: () => FormContext<TEntity>;
         getEventSource: () => any;
@@ -85,7 +10,7 @@ declare global
     };
 
     type AnyEntity = {
-        [key: string]: AnyAttribute;
+        [key: string]: Attribute;
     };
 
     type FormContext<TEntity extends AnyEntity = AnyEntity> = {
@@ -93,6 +18,32 @@ declare global
         getControl: <TField extends keyof TEntity> (id: TField) => Control;
         data: Data;
         ui: Ui<TEntity>;
+    };
+
+    type Control = {
+        getValue: () => string;
+        /** If set to true, the control will not be editable */
+        setDisabled: (disabled: boolean) => void;
+        getVisible: () => boolean;
+        /** If set to false, the control will not be shown */
+        setVisible: (visible: boolean) => void;
+        setLabel: (label: string) => void;
+        setFocus: () => void;
+        addNotification: (options: XrmNotificationOptions) => void;
+        clearNotification: (id: string) => void;
+        getOptions: () => Option[];
+        addOption: (option: Option, index?: number) => void;
+        removeOption: (value: number) => void;
+        clearOptions: () => void;
+        addCustomFilter: (filter: string, entityLogicalName?: string) => void;
+        addPreSearch: (onPreSearch: () => void) => void;
+        addCustomView: (
+            viewId: string,
+            entityName: string,
+            viewDisplayName: string,
+            fetchXml: string,
+            layoutXml: string,
+            isDefault: boolean) => void;
     };
 
     type Data = {
@@ -208,93 +159,9 @@ declare global
         setVisible: (visible: boolean) => void;
     };
 
-    type Option = {
-        text: string;
-        value: number;
-    };
-
     type RequirementLevel = "none" | "required" | "recommended";
 
     type UserPriviledge = { canRead: boolean, canUpdate: boolean, canCreate: boolean; };
-
-    type AnyAttribute = {
-        getValue: () => any;
-        setValue: (value: any) => void;
-        addOnChange: (onChange: (context: ExecutionContext) => void) => void;
-        controls: Control[];
-        setSubmitMode: (mode: "always" | "never" | "dirty") => void;
-        getPartyList: () => boolean;
-        getMax: () => number;
-        getMin: () => number;
-        getName: () => string;
-        getRequiredLevel: () => RequirementLevel;
-        setRequiredLevel: (requirementLevel: RequirementLevel) => void;
-        getUserPriviledge: () => UserPriviledge;
-        fireOnChange: () => void;
-        getIsDirty: () => boolean;
-    };
-
-    type LookupAttribute = Override<AnyAttribute, {
-        getValue: () => { id: string; }[];
-        setValue: (value: {
-            entityType: string,
-            id: string,
-            name: string;
-        }[]) => void;
-    }>;
-
-    type StringAttribute = Override<AnyAttribute, {
-        getValue: () => string;
-        setValue: (value: string) => void;
-    }>;
-
-    type NumberAttribute = Override<AnyAttribute, {
-        getValue: () => number;
-        setValue: (value: number) => void;
-    }>;
-
-    type BooleanAttribute = Override<AnyAttribute, {
-        getValue: () => boolean;
-        setValue: (value: boolean) => void;
-    }>;
-
-    type DateAttribute = Override<AnyAttribute, {
-        getValue: () => Date;
-        setValue: (value: Date) => void;
-    }>;
-
-    type OptionSetAttribute = Override<AnyAttribute, {
-        getValue: () => number;
-        setValue: (value: number) => void;
-        getOption: () => Option;
-        getOptions: () => Option[];
-    }>;
-
-    type Control = {
-        getValue: () => string;
-        /** If set to true, the control will not be editable */
-        setDisabled: (disabled: boolean) => void;
-        getVisible: () => boolean;
-        /** If set to false, the control will not be shown */
-        setVisible: (visible: boolean) => void;
-        setLabel: (label: string) => void;
-        setFocus: () => void;
-        addNotification: (options: XrmNotificationOptions) => void;
-        clearNotification: (id: string) => void;
-        getOptions: () => Option[];
-        addOption: (option: Option, index?: number) => void;
-        removeOption: (value: number) => void;
-        clearOptions: () => void;
-        addCustomFilter: (filter: string, entityLogicalName?: string) => void;
-        addPreSearch: (onPreSearch: () => void) => void;
-        addCustomView: (
-            viewId: string,
-            entityName: string,
-            viewDisplayName: string,
-            fetchXml: string,
-            layoutXml: string,
-            isDefault: boolean) => void;
-    };
 
     type XrmNotificationOptions = {
         uniqueId: string;
